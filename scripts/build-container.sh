bb043092ef7c1f6a898843ef27d5ccccdd1b5b0f
Amir Nassereldine <46420889+amirnd51@users.noreply.github.com>
2024-07-23 21:05:09 -0400

    Update build-container.sh

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -36,7 +36,7 @@ if [ ! -f "$DOCKERFILE" ]; then
     die_with_message "Dockerfile ${DOCKERFILE} does not exist"
 fi
 
-docker build -t "mlmodelscope-$TARGET:$VERSION" --file "$DOCKERFILE" "$TARGET_DIR"
-docker tag "mlmodelscope-$TARGET:$VERSION" "mlmodelscope-$TARGET:latest"
+docker build -t "mlmodelscope-api:$VERSION" --file "$DOCKERFILE" "$TARGET_DIR"
+docker tag "mlmodelscope-api:$VERSION" "mlmodelscope-api:latest"
 
 cleanup_and_exit
